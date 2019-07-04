ad93acadb2bda43e4822aded42f89ddd86c64eb2
muhasyaifudin <muha.syaifudin@gmail.com>
2019-07-04 21:13:23 +0700

    init

---
+++ application/controllers/LoginController.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class LoginController extends MY_Controller {
+
+	public function index()
+	{
+			
+	}
+
+}
+
+/* End of file LoginController.php */
+/* Location: ./application/controllers/LoginController.php */
---
+++ application/core/MY_Controller.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MY_Controller extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		
+	}
+
+	public function load_view($v, $data = [])
+	{
+		
+		$data = array(
+			// "title" => $v['title'],
+			"content" => $v,
+			"data" => $data,
+		);	
+
+		$this->load->view('master', $data);
+	}
+
+}
+
+/* End of file MY_Controller.php */
+/* Location: ./application/core/MY_Controller.php */
